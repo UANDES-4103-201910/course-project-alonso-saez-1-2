a8f3dfeb5201f1c843f650f48932279c3491cf9d
Joaquin Alonso <jalonso1@miuandes.cl>
2019-04-11 15:22:16 -0400

    Modelos

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
+  get '/posts/:id' => "posts#show"
+  post '/posts' => "posts#create"
+  put '/posts/:id' => "posts#update"
+  delete '/posts/:id' => "posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ project/db/migrate/20190411191711_create_join_table_user_inapropiate_post.rb
@@ -0,0 +1,4 @@
+class CreateJoinTableUserInapropiatePost < ActiveRecord::Migration[5.2]
+  def change
+  end
+end

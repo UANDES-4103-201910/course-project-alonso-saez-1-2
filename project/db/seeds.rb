4c3b3cba2e5495855ea0d1dfede4af171fff610f
AntoniaSaez <asaez1@miuandes.cl>
2019-05-10 23:45:09 -0400

    listo

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -9,3 +9,5 @@ SuperAdmin.create(profile_image:'imagen super admin', nickname:'Antonia Saez' ,b
 Admin.create(profile_image:'imagen admin', nickname:'Joaquin Alonso' ,biography:'Master of complain' ,email:'jalonso1@miuandes.cl' ,password:'123456789' ,country:'Chile' ,city:'Santiago' ,status:'Available' ,gps_location:'My home', super_admin_id: 1)
 User.create(profile_image:'imagen user', nickname:'Matias Jijena' ,biography:'Master of complain' ,email:'mjijena@miuandes.cl' ,password:'123456789' ,country:'Chile' ,city:'Santiago' ,status:'Available' ,gps_location:'My home', admin_id: 1 ,super_admin_id: 1)
 Wall.create()
+Post.create(title:'I hate almost everyone',creation_date:'2019-05-10',solve_status:'Solved',file:'aaa',image:'eee',text:'I only hate stupid people',link:'iii',wall_id: 1, super_admin_id: 1, admin_id: 1, user_id: 1)
+Post.create(title:'I hate traffic',creation_date:'2019-05-10',solve_status:'Solved',file:'aaa',image:'eee',text:'Am I the only one who hate Santiago  traffic?',link:'iii',wall_id: 1, super_admin_id: 1, admin_id: 1, user_id: 1)

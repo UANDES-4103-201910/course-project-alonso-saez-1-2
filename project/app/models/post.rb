20b4e26083b3c8d92d76d75c334afb426e5d5019
AntoniaSaez <asaez1@miuandes.cl>
2019-05-25 22:38:28 -0400

    File posts

--- project/app/models/post.rb
+++ project/app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :admin
   belongs_to :user
   has_many :comments
+  has_one_attached :image
   #belongs_to :users
   #belongs_to :dumpsters
   validates :text, length: {minimum: 1, :message => 'Your text has less than 1 characters'}

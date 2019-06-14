b1d028712753c0b410f9940d73aacb3a36cc97ff
AntoniaSaez <asaez1@miuandes.cl>
2019-06-13 23:24:52 -0400

    search bar finally de los finallys

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where(["nickname LIKE ?","%#{params[:search2]}%"])
   end
 
   # GET /users/1
